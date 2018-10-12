24e320c02b0fe3bc2983dbc868f2d3d892cb7a8a
NjeriNjoroge <wanjorogegrace@gmail.com>
2018-10-12 19:49:59 +0300

    app fetches from Firebase and displays users

--- ChatApp/NewMessageController.swift
+++ ChatApp/NewMessageController.swift
@@ -7,12 +7,73 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class NewMessageController: UITableViewController {
+    
+    let cellId = "cellId"
+    
+    var users = [User]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(handleCancel))
 
+        tableView.register(UserCell.self, forCellReuseIdentifier: cellId)
+        
+        fetchUser()
+    }
+    
+    func fetchUser() {
+        Database.database().reference().child("users").observe(.childAdded, with: { (snapshot) in
+            
+            if let dictionary = snapshot.value as? [String: AnyObject] {
+                let user = User()
+                user.name = dictionary["name"] as? String
+                user.email = dictionary["email"] as? String
+                print(user.name, user.email)
+                
+                self.users.append(user)
+                
+                //put it in a dispatch so that it cannot crash due to background thread
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+ 
+        }, withCancel: nil)
+    }
+    
+    @objc func handleCancel() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        //need to dequeue cells for memory efficiency. Remove this hack
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        
+        let user = users[indexPath.row]
+        print(user)
+        cell.textLabel?.text = user.name
+        cell.detailTextLabel?.text = user.email
+        
+        return cell
     }
 
+}
+
+class UserCell: UITableViewCell {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
---
+++ ChatApp/User.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  ChatApp
+//
+//  Created by Grace Njoroge on 12/10/2018.
+//  Copyright © 2018 Grace Njoroge. All rights reserved.
+//
+
+import Foundation
+
+
+class User: NSObject {
+    var name: String?
+    var email: String?
+    
+}
